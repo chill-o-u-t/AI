528a6c2d37672c127d9589ed02785c7a94f55962
Морозов Иван Денисович <i.morozov@nicetu.spb.ru>
2023-05-15 11:35:33 +0300

    nt, nt, nt

--- predict.py
+++ predict.py
@@ -1,8 +1,6 @@
 import os
-from datetime import time
 
 import easyocr
-from yolo_ai.models import Yolov4
 from PIL import Image
 
 FIRST = False
@@ -10,19 +8,6 @@ PATH = 'base_dir'
 
 if FIRST:
     os.mkdir(PATH)
-
-
-def load_model_yolo(model_path: str, classes_path: str) -> Yolov4:
-    try:
-        model = Yolov4(
-            weight_path=model_path,
-            class_name_path=classes_path
-            # weight_path='yolov4-obj_best.weights',
-            # class_name_path='classes.txt'
-        )
-    except Exception as error:
-        raise 'При попытке загрузить модель yolov4 возникла ошибка {error}'.format(error=error)
-    return model
 
 
 def load_model_easyocr(
@@ -42,82 +27,21 @@ def load_model_easyocr(
     return reader
 
 
-def check_image(image_path: str) -> str:
-    name, expansion = image_path.split('.')
-    if expansion != 'jpg':
-        image = Image.open(f'{PATH}/{image_path}').convert('RGB')
-        image.save(f'{PATH}/{name}.jpg')
-        os.remove(f'{PATH}/{image_path}')
-        return f'{name}.jpg'
-    return image_path
+if __name__ == '__main__':
+    import easyocr
 
-
-def get_images_after_yolo(yolo_objects, image_path: str) -> None:
-    os.mkdir(f'{PATH}/result')
-    image = Image.open(f'{PATH}/{image_path}')
-    for obj in yolo_objects.values:
-        extracted_image = image.crop(
-            (int(obj[0]), int(obj[1]), int(obj[2]), int(obj[3]))
-        )
-        extracted_image.save(f'{PATH}/result/{obj[4]}.jpg')
-
-
-def rotate_image() -> None:
-    image = Image.open(f'{PATH}/result/series.jpg')
-    img = image.rotate(90, expand=True)
-    img.save(f'{PATH}/result/_series.jpg')
-    os.remove(f'{PATH}/result/series.jpg')
-
-
-def get_text_from_image(image_file: str, reader: easyocr.Reader):
-    return reader.readtext(image_file)
-
-
-def get_text(reader: easyocr.Reader) -> dict:
-    result = {}
-    for file in os.listdir(f'{PATH}/result'):
-        text = get_text_from_image(
-            f'{PATH}/result/{file}',
-            reader=reader
-        )
-        result[f'{file.split(".")[0]}'] = [r[1] + ' ' for r in text]
-    return result
-
-
-def main():
-    model = load_model_yolo(
-        model_path='models/yolov4-obj_best.weights',
-        classes_path='classes.txt'
-    )
     reader = load_model_easyocr(
         'custom_EasyOCR/model',
         'custom_EasyOCR/user_network',
         'custom_example'
     )
-    img_path = 'obj682.jpg'
-    img_path = check_image(img_path)
-    try:
-        yolo_objects = model.predict(f'{PATH}/{img_path}')
-        print(yolo_objects)
-    except Exception as error:
-        raise ValueError(f'{error}')
-    get_images_after_yolo(yolo_objects, img_path)
-    rotate_image()
-    result = get_text(reader)
-    for key, value in result.items():
-        print(key, str(value).upper())
-
-
-if __name__ == '__main__':
-    main()
-    """reader = load_model_easyocr(
-        'custom_EasyOCR/model',
-        'custom_EasyOCR/user_network',
-        'custom_example'
-    )
-    image = 'nmyxiC2S1gc.jpg'
+    image = 'base_dir/result/date.jpg'
     result = reader.readtext(image)
     for r in result:
-        print(r[1])"""
+        print(r[1].lower())
+    image = 'base_dir/result/city.jpg'
+    result = reader.readtext(image)
+    for r in result:
+        print(r[1].lower())
 
 
